ad0cc4a7e2f9a1250e3a645a3b4f6c11c6830918
dct <dct@B-S>
2021-03-22 14:28:57 +0200

    EventHandler constructing

---
+++ CollisionManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PolimorphismApp
+{
+    class CollisionManager
+    {
+        // Step #2: Define the event member
+        public event EventHandler<NewCollisionEventArgs> NewCollision;
+
+        // Step #3: Define a method responsible for raising the event
+        // to notify registered objects that the event has occurred       
+        protected virtual void OnNewCollision(NewCollisionEventArgs e)
+        {
+            EventHandler<NewCollisionEventArgs> temp = Volatile.Read(ref NewCollision);
+            if (temp != null)
+            {
+                temp(sender: this, e);
+            }
+        }
+
+        // Step #4: Define a method that translates the
+        // input into the desired event
+        public void SimulateCollision(int X, int Y)
+        {
+            NewCollisionEventArgs e = new NewCollisionEventArgs(X, Y);
+            OnNewCollision(e);
+        }
+    }
+}
---
+++ NewCollisionEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolimorphismApp
+{
+	// Step #1: Define a type that will hold any additional information that
+	// should be sent to receivers of the event notification
+	class NewCollisionEventArgs
+    {
+		
+		private readonly int X, Y;
+		public NewCollisionEventArgs(int X, int Y)
+		{
+			this.X = X;
+			this.Y = Y;
+		}
+		public int X_Collision { get { return this.X; } }
+		public int Y_Collision { get { return this.Y; } }
+		
+	}
+}
